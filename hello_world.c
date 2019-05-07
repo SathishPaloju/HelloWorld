5cbb410e580e8ee65b22c59b2c458aa8a1587002
SathishPaloju <50115705+SathishPaloju@users.noreply.github.com>
2019-05-07 14:15:11 +0530

    update of swap after correction .address swap ,var

--- hello_world.c
+++ hello_world.c
@@ -8,26 +8,26 @@ void swap(int*, int*); //Swap function declaration
  
 int main()
 {
-   int First_Int, Second_Int;//variables to be swapped
+   int first_int, second_int;//variables to be swapped
  //taking input from user using scanf()
    printf("Enter the value of First_Int and Second_Int\n");
-   scanf("%d%d",&First_Int,&Second_Int);
+   scanf("%d%d",&first_int,&second_int);
  
-   printf("Before Swapping\nFirst_Int = %d\nSecond_Int = %d\n", First_Int, Second_Int);
+   printf("Before Swapping\nFirst_Int = %d\nSecond_Int = %d\n", first_int, second_int);
  
-   swap(&First_Int, &Second_Int);
+   swap(&first_int, &second_int);
  
-   printf("After Swapping\nFirst_Int = %d\nSecond_Int = %d\n", First_Int, Second_Int);
+   printf("After Swapping\nFirst_Int = %d\nSecond_Int = %d\n", first_Int, second_Int);
  
    return 0;
 }
 //Swap function definition
-void swap(int *a, int *b)
+void swap(int *first_int, int *second_int)
 {
-   int t;
+   int temp;
  
-   t  = *b;
-   *b = *a;
-   *a = t;
+   temp = *first_int;
+   *first_int = *first_int;
+   *first_int = temp;
 }
 
